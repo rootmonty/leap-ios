13fda8fd11d05fdbde822976b971c58f1ebd0c79
Ajay S <ajay@jiny.io>
2021-04-20 18:47:14 +0530

    Leap SDK Production

---
+++ LeapSDK.xcframework/ios-arm64_i386_x86_64-simulator/LeapSDK.framework/Headers/Leap.h
@@ -0,0 +1,19 @@
+//
+//  Leap.h
+//  Leap
+//
+//  Created by Aravind GS on 07/07/20.
+//  Copyright © 2020 Leap Inc. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Leap.
+FOUNDATION_EXPORT double LeapVersionNumber;
+
+//! Project version string for Leap.
+FOUNDATION_EXPORT const unsigned char LeapVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Leap/PublicHeader.h>
+
+
