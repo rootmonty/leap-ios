5cf3defc4de08b69a1086d083ac3366980eb0848
Ajay S <ajay@jiny.io>
2021-08-25 21:23:55 +0530

    Added AppConstants file

---
+++ LeapPreview/AppConstants.swift
@@ -0,0 +1,21 @@
+//
+//  AppConstants.swift
+//  Leap Preview
+//
+//  Created by Ajay S on 25/08/21.
+//
+
+import Foundation
+
+let constant_sampleAppInfoDict = "sampleAppInfoDict"
+let constant_platformType = "platformType"
+let constant_IOS = "IOS"
+let constant_owner = "owner"
+let constant_LEAP = "LEAP"
+let constant_apiKey = "apiKey"
+let constant_webUrl = "webUrl"
+let constant_QRErrorTitle = "QR not matched"
+let constant_QRErrorMessage = "Please scan a QR for an iOS App"
+let constant_OK = "OK"
+let constant_sampleAppRescan = "sampleAppRescan"
+let constant_rescan = "rescan"
